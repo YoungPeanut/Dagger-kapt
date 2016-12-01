d0e6c3b54ad500b478f5508cdd86c08261f92e55
ChenShao_sina <chenshao@staff.sina.com.cn>
2016-12-01 13:51:13 +0800

    readme

--- app/src/main/java/io/github/youngpeanut/dagger/DaggerActivity.kt
+++ app/src/main/java/io/github/youngpeanut/dagger/DaggerActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import javax.inject.Inject
 
+/**
+ * 为什么会要用Dagger？
+ *
+ * DaggerActivity依赖presenter，当然可以直接new DaggerPresenter()得到，但是DaggerPresenter改变的时候，DaggerActivity也得修改。
+ * Dagger利用依赖注入的方式解决这个问题。
+ */
 class DaggerActivity : AppCompatActivity() {
 
     @Inject
